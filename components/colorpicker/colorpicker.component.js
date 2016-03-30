7a2d4377578f2f3546bde7c75759d85fcf0586f8
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2016-03-30 16:50:57 +0200

    component colorpicker implemented

---
+++ components/colorpicker/colorpicker.component.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// Naming conventions! 
+// Prefixing!
+// Never access scope without a dot!
+
+angular.module('myApp.colorpicker', [])
+    .component('colorpicker', {
+        bindings: {
+            r: '@red',
+            a: '=alpha' // two-way binding as 'alpha' as external API parameter 
+        },
+        // import the template from a systemjs module directly if deployed 
+        templateUrl: 'components/colorpicker/colorpicker.component.html',
+        controller: function() {
+            // this binds on controllerAs (by default "$ctrl")
+            // you should not use $scope directly
+            this.r = 255
+            this.g = 30
+            this.b = 0
+            this.a = 0.5
+        }
+    })
